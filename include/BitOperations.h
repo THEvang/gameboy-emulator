75526071ae1d1e8e05c796885f79ea3a5bb995de
Thomas <thomasevang@gmail.com>
2020-02-22 15:23:43 +0100

    Functions for setting, clearing and testing bits. Half carry test

--- include/BitOperations.h
+++ include/BitOperations.h
@@ -14,4 +14,10 @@ T rotate_right(T value, int n) {
     return (value >> n) | (value << (n - sizeof(T) * 8));
 }
 
+void set_bit(uint8_t& b, int n);
+void clear_bit(uint8_t& b, int n);
+
+bool is_set(uint8_t b, int n);
+
 uint16_t combine_bytes(uint8_t upper, uint8_t lower);
+bool half_carry(uint8_t a, uint8_t b);
--- src/Utilities/BitOperations.cpp
+++ src/Utilities/BitOperations.cpp
@@ -3,3 +3,19 @@
 uint16_t combine_bytes(uint8_t upper, uint8_t lower) {
     return (upper << 8) | lower;
 }
+
+bool half_carry(uint8_t a, uint8_t b) {
+    return (((a & 0xF) + (b & 0xF)) & 0x10) == 0x10;
+}
+
+void set_bit(uint8_t& b, int n) {
+    b |= (1 << n);
+}
+
+void clear_bit(uint8_t& b, int n) {
+    b &= ~(1 << n);
+}
+
+bool is_set(uint8_t b, int n) {
+    return ((1 << n) & b) != 0;
+}
