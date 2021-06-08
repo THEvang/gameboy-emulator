945a7c05c9864d18d9c5b62143faec1202dde71a
Thomas H. Evang <thomasevang@gmail.com>
2021-06-09 00:14:40 +0200

    Removed unused file

--- src/Memory/Special_Addresses.hpp
+++
@@ -1,9 +0,0 @@
-#include <cstdint>
-
-namespace Special_Addresses {
-
-    constexpr uint16_t Interrupt_request = 0xFF0F;
-    constexpr uint16_t Interrupt_Enable = 0xFFFF;
-    constexpr uint16_t LCD_Control = 0xFF40;
-}
-  
