90a024bae290f00a8a3c42c39c5714007b54acef
Thomas H. Evang <thomasevang@gmail.com>
2021-06-09 23:57:42 +0200

    Moved ram toggling out of MemoryBankClass

--- src/Memory/Memory_Controller.hpp
+++ src/Memory/Memory_Controller.hpp
@@ -12,18 +12,17 @@ public:
 
     MemoryBankController(uint8_t* cartridge_memory);
 
-    [[nodiscard]] uint8_t read(uint16_t address) const;
+    uint8_t read(uint16_t address) const;
     void write(uint16_t address, uint8_t data);
 
-    uint8_t internal_memory[0xFFFF];
+    uint8_t memory[0xFFFF];
+    uint8_t* rom;
+    bool ram_enabled = false;
 
 private:
 
-    Bank_Type m_bank_type = Bank_Uknown;
-
     uint8_t m_ram_bank = 1;
     uint8_t m_rom_bank = 1;
-    bool m_ram_enabled = false;
     BankingMode m_banking_mode = Banking_Mode_ROM;
 
     void special_writes(uint16_t address);
@@ -35,11 +34,11 @@ private:
 
     void switch_banking_mode(uint8_t data);
 
-    void ram_switching(uint8_t data);
     void set_lower_rom_bank_number(uint8_t data);
     void set_upper_rom_bank_number(uint8_t data);
     void set_ram_bank_number(uint8_t data);
     void set_banking_mode(uint8_t data);
 
-    uint8_t* m_cartridge_memory;
 };
+
+void toggle_ram(MemoryBankController* mc, uint8_t data);
