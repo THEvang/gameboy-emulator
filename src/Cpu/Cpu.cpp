61ec3c224fae5995583a75e0e09279eb40e2ae23
Thomas <thomasevang@gmail.com>
2021-06-09 18:57:56 +0200

    Replaced CPU constructor with free function

--- src/Cpu/Cpu.cpp
+++ src/Cpu/Cpu.cpp
@@ -6,55 +6,53 @@ uint16_t read_register_pair(Cpu cpu, Cpu_Register r1, Cpu_Register r2) {
     return combine_bytes(cpu.registers[r1], cpu.registers[r2]);
 }
 
-Cpu::Cpu(MemoryBankController* memory_controller)
-    : memory_controller(memory_controller)
-{
-    registers[Register_A] = 0x01;
-    registers[Register_B] = 0x00;
-    registers[Register_C] = 0x13;
-    registers[Register_D] = 0x00;
-    registers[Register_E] = 0xD8;
-    registers[Register_F] = 0xB0;
-    registers[Register_H] = 0x01;
-    registers[Register_L] = 0x4D;
+void set_initial_state(Cpu* cpu) {
+    cpu->registers[Register_A] = 0x01;
+    cpu->registers[Register_B] = 0x00;
+    cpu->registers[Register_C] = 0x13;
+    cpu->registers[Register_D] = 0x00;
+    cpu->registers[Register_E] = 0xD8;
+    cpu->registers[Register_F] = 0xB0;
+    cpu->registers[Register_H] = 0x01;
+    cpu->registers[Register_L] = 0x4D;
 
-    memory_controller->write(0xFF05, 0);
-    memory_controller->write(0xFF06, 0);
-    memory_controller->write(0xFF07, 0);
+    cpu->memory_controller->write(0xFF05, 0);
+    cpu->memory_controller->write(0xFF06, 0);
+    cpu->memory_controller->write(0xFF07, 0);
 
-    memory_controller->write(0xFF10, 0x80);
-    memory_controller->write(0xFF11, 0xBF);
-    memory_controller->write(0xFF12, 0xF3);
+    cpu->memory_controller->write(0xFF10, 0x80);
+    cpu->memory_controller->write(0xFF11, 0xBF);
+    cpu->memory_controller->write(0xFF12, 0xF3);
 
-    memory_controller->write(0xFF14, 0xBF);
-    memory_controller->write(0xFF16, 0x3F);
-    memory_controller->write(0xFF17, 0x00);
-    memory_controller->write(0xFF19, 0xBF);
+    cpu->memory_controller->write(0xFF14, 0xBF);
+    cpu->memory_controller->write(0xFF16, 0x3F);
+    cpu->memory_controller->write(0xFF17, 0x00);
+    cpu->memory_controller->write(0xFF19, 0xBF);
 
-    memory_controller->write(0xFF1A, 0x7F);
-    memory_controller->write(0xFF1B, 0xFF);
-    memory_controller->write(0xFF1C, 0x9F);
-    memory_controller->write(0xFF1E, 0xBF);
+    cpu->memory_controller->write(0xFF1A, 0x7F);
+    cpu->memory_controller->write(0xFF1B, 0xFF);
+    cpu->memory_controller->write(0xFF1C, 0x9F);
+    cpu->memory_controller->write(0xFF1E, 0xBF);
 
-    memory_controller->write(0xFF20, 0xFF);
-    memory_controller->write(0xFF21, 0x00);
-    memory_controller->write(0xFF22, 0x00);
-    memory_controller->write(0xFF23, 0xBF);
-    memory_controller->write(0xFF24, 0x77);
-    memory_controller->write(0xFF25, 0xF3);
-    memory_controller->write(0xFF26, 0xF1);
+    cpu->memory_controller->write(0xFF20, 0xFF);
+    cpu->memory_controller->write(0xFF21, 0x00);
+    cpu->memory_controller->write(0xFF22, 0x00);
+    cpu->memory_controller->write(0xFF23, 0xBF);
+    cpu->memory_controller->write(0xFF24, 0x77);
+    cpu->memory_controller->write(0xFF25, 0xF3);
+    cpu->memory_controller->write(0xFF26, 0xF1);
 
-    memory_controller->write(0xFF40, 0x91);
-    memory_controller->write(0xFF42, 0x00);
-    memory_controller->write(0xFF43, 0x00);
-    memory_controller->write(0xFF45, 0x00);
-    memory_controller->write(0xFF47, 0xFC);
-    memory_controller->write(0xFF48, 0xFF);
-    memory_controller->write(0xFF49, 0xFF);
+    cpu->memory_controller->write(0xFF40, 0x91);
+    cpu->memory_controller->write(0xFF42, 0x00);
+    cpu->memory_controller->write(0xFF43, 0x00);
+    cpu->memory_controller->write(0xFF45, 0x00);
+    cpu->memory_controller->write(0xFF47, 0xFC);
+    cpu->memory_controller->write(0xFF48, 0xFF);
+    cpu->memory_controller->write(0xFF49, 0xFF);
 
-    memory_controller->write(0xFF4A, 0x00);
-    memory_controller->write(0xFF4B, 0x00);
-    memory_controller->write(0xFFFF, 0x00);
+    cpu->memory_controller->write(0xFF4A, 0x00);
+    cpu->memory_controller->write(0xFF4B, 0x00);
+    cpu->memory_controller->write(0xFFFF, 0x00);
 }
 
 bool test_flag(uint8_t flags, Cpu_Flag flag) {
