a327e3bb8c1ba67737ddf14ab366b99f11dde609
Thomas H. Evang <thomasevang@gmail.com>
2021-06-12 17:56:41 +0200

    added gb prefix writes in CPU

--- src/Cpu/Cpu.c
+++ src/Cpu/Cpu.c
@@ -26,43 +26,43 @@ void set_initial_state(Cpu* cpu) {
     cpu->registers[Register_H] = 0x01;
     cpu->registers[Register_L] = 0x4D;
 
-    write(cpu->memory_controller, 0xFF05, 0);
-    write(cpu->memory_controller, 0xFF06, 0);
-    write(cpu->memory_controller, 0xFF07, 0);
+    gb_write(cpu->memory_controller, 0xFF05, 0);
+    gb_write(cpu->memory_controller, 0xFF06, 0);
+    gb_write(cpu->memory_controller, 0xFF07, 0);
 
-    write(cpu->memory_controller, 0xFF10, 0x80);
-    write(cpu->memory_controller, 0xFF11, 0xBF);
-    write(cpu->memory_controller, 0xFF12, 0xF3);
+    gb_write(cpu->memory_controller, 0xFF10, 0x80);
+    gb_write(cpu->memory_controller, 0xFF11, 0xBF);
+    gb_write(cpu->memory_controller, 0xFF12, 0xF3);
 
-    write(cpu->memory_controller, 0xFF14, 0xBF);
-    write(cpu->memory_controller, 0xFF16, 0x3F);
-    write(cpu->memory_controller, 0xFF17, 0x00);
-    write(cpu->memory_controller, 0xFF19, 0xBF);
+    gb_write(cpu->memory_controller, 0xFF14, 0xBF);
+    gb_write(cpu->memory_controller, 0xFF16, 0x3F);
+    gb_write(cpu->memory_controller, 0xFF17, 0x00);
+    gb_write(cpu->memory_controller, 0xFF19, 0xBF);
 
-    write(cpu->memory_controller, 0xFF1A, 0x7F);
-    write(cpu->memory_controller, 0xFF1B, 0xFF);
-    write(cpu->memory_controller, 0xFF1C, 0x9F);
-    write(cpu->memory_controller, 0xFF1E, 0xBF);
+    gb_write(cpu->memory_controller, 0xFF1A, 0x7F);
+    gb_write(cpu->memory_controller, 0xFF1B, 0xFF);
+    gb_write(cpu->memory_controller, 0xFF1C, 0x9F);
+    gb_write(cpu->memory_controller, 0xFF1E, 0xBF);
 
-    write(cpu->memory_controller, 0xFF20, 0xFF);
-    write(cpu->memory_controller, 0xFF21, 0x00);
-    write(cpu->memory_controller, 0xFF22, 0x00);
-    write(cpu->memory_controller, 0xFF23, 0xBF);
-    write(cpu->memory_controller, 0xFF24, 0x77);
-    write(cpu->memory_controller, 0xFF25, 0xF3);
-    write(cpu->memory_controller, 0xFF26, 0xF1);
+    gb_write(cpu->memory_controller, 0xFF20, 0xFF);
+    gb_write(cpu->memory_controller, 0xFF21, 0x00);
+    gb_write(cpu->memory_controller, 0xFF22, 0x00);
+    gb_write(cpu->memory_controller, 0xFF23, 0xBF);
+    gb_write(cpu->memory_controller, 0xFF24, 0x77);
+    gb_write(cpu->memory_controller, 0xFF25, 0xF3);
+    gb_write(cpu->memory_controller, 0xFF26, 0xF1);
 
-    write(cpu->memory_controller, 0xFF40, 0x91);
-    write(cpu->memory_controller, 0xFF42, 0x00);
-    write(cpu->memory_controller, 0xFF43, 0x00);
-    write(cpu->memory_controller, 0xFF45, 0x00);
-    write(cpu->memory_controller, 0xFF47, 0xFC);
-    write(cpu->memory_controller, 0xFF48, 0xFF);
-    write(cpu->memory_controller, 0xFF49, 0xFF);
+    gb_write(cpu->memory_controller, 0xFF40, 0x91);
+    gb_write(cpu->memory_controller, 0xFF42, 0x00);
+    gb_write(cpu->memory_controller, 0xFF43, 0x00);
+    gb_write(cpu->memory_controller, 0xFF45, 0x00);
+    gb_write(cpu->memory_controller, 0xFF47, 0xFC);
+    gb_write(cpu->memory_controller, 0xFF48, 0xFF);
+    gb_write(cpu->memory_controller, 0xFF49, 0xFF);
 
-    write(cpu->memory_controller, 0xFF4A, 0x00);
-    write(cpu->memory_controller, 0xFF4B, 0x00);
-    write(cpu->memory_controller, 0xFFFF, 0x00);
+    gb_write(cpu->memory_controller, 0xFF4A, 0x00);
+    gb_write(cpu->memory_controller, 0xFF4B, 0x00);
+    gb_write(cpu->memory_controller, 0xFFFF, 0x00);
 }
 
 bool test_flag(uint8_t flags, Cpu_Flag flag) {
