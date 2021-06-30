769eaa319fb3d7898548e2f4e175592e3c83863a
Thomas H. Evang <thomasevang@gmail.com>
2021-06-30 23:02:19 +0200

    Moved event polling to render loop, because polling is slow on linux

--- src/App/gb-emu.c
+++ src/App/gb-emu.c
@@ -22,88 +22,90 @@ void render_main(GameBoy* gameboy) {
     bool done = false;
     while (!done) {
 
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            switch (event.type) {
-            
-            case SDL_QUIT:
-                done = true;
-                break;
-            
-            case SDL_WINDOWEVENT:
-                if(event.window.event == SDL_WINDOWEVENT_CLOSE) {
-                    done = true;
-                }
-                break;
-            
-            case SDL_KEYDOWN:
-                switch(event.key.keysym.sym) {
-                    case SDLK_z:
-                        input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Down, Button_A);
-                        break;
-                    case SDLK_x:
-                        input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Down, Button_B);
-                        break;
-                    case SDLK_RETURN:
-                        input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Down, Button_Start);
-                        break;
-                    case SDLK_BACKSPACE:
-                        input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Down, Button_Select);
-                        break;
-                    case SDLK_w:
-                        input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Down, Button_Up);
-                        break;
-                    case SDLK_s:
-                        input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Down, Button_Down);
-                        break;
-                    case SDLK_d:
-                        input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Down, Button_Right);
-                        break;
-                    case SDLK_a:
-                        input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Down, Button_Left);
-                        break;
-                }
-            break;
-
-            case SDL_KEYUP:
-                switch(event.key.keysym.sym) {
-                    case SDLK_z:
-                        input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Up, Button_A);
-                        break;
-                    case SDLK_x:
-                        input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Up, Button_B);
-                        break;
-                    case SDLK_RETURN:
-                        input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Up, Button_Start);
-                        break;
-                    case SDLK_BACKSPACE:
-                        input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Up, Button_Select);
-                        break;
-                    case SDLK_w:
-                        input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Up, Button_Up);
-                        break;
-                    case SDLK_s:
-                        input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Up, Button_Down);
-                        break;
-                    case SDLK_d:
-                        input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Up, Button_Right);
-                        break;
-                    case SDLK_a:
-                        input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Up, Button_Left);
-                        break;
-                }
-            break;
-
-            default:
-                break;
-            }
-        }
-
+        
         gb_run(gameboy);
         double duration =  (double) (stop - start) / CLOCKS_PER_SEC;
         if( duration >= 16e-3) {
             start = clock();
+
+                SDL_Event event;
+            while (SDL_PollEvent(&event))
+            {
+                switch (event.type) {
+                
+                case SDL_QUIT:
+                    done = true;
+                    break;
+                
+                case SDL_WINDOWEVENT:
+                    if(event.window.event == SDL_WINDOWEVENT_CLOSE) {
+                        done = true;
+                    }
+                    break;
+                
+                case SDL_KEYDOWN:
+                    switch(event.key.keysym.sym) {
+                        case SDLK_z:
+                            input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Down, Button_A);
+                            break;
+                        case SDLK_x:
+                            input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Down, Button_B);
+                            break;
+                        case SDLK_RETURN:
+                            input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Down, Button_Start);
+                            break;
+                        case SDLK_BACKSPACE:
+                            input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Down, Button_Select);
+                            break;
+                        case SDLK_w:
+                            input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Down, Button_Up);
+                            break;
+                        case SDLK_s:
+                            input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Down, Button_Down);
+                            break;
+                        case SDLK_d:
+                            input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Down, Button_Right);
+                            break;
+                        case SDLK_a:
+                            input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Down, Button_Left);
+                            break;
+                    }
+                break;
+
+                case SDL_KEYUP:
+                    switch(event.key.keysym.sym) {
+                        case SDLK_z:
+                            input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Up, Button_A);
+                            break;
+                        case SDLK_x:
+                            input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Up, Button_B);
+                            break;
+                        case SDLK_RETURN:
+                            input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Up, Button_Start);
+                            break;
+                        case SDLK_BACKSPACE:
+                            input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Up, Button_Select);
+                            break;
+                        case SDLK_w:
+                            input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Up, Button_Up);
+                            break;
+                        case SDLK_s:
+                            input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Up, Button_Down);
+                            break;
+                        case SDLK_d:
+                            input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Up, Button_Right);
+                            break;
+                        case SDLK_a:
+                            input(&(gameboy->joypad), gameboy->memory_bank_controller, Key_Up, Button_Left);
+                            break;
+                    }
+                break;
+
+                default:
+                    break;
+                }
+            }
+
             gb_render(&gui, gameboy);
         }
 
