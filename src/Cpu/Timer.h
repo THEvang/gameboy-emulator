a96aff5541eae2503e9ce6b56174404f2929a478
Thomas H. Evang <thomasevang@gmail.com>
2021-06-30 14:32:06 +0200

    Added field for tima speed

--- src/Cpu/Timer.h
+++ src/Cpu/Timer.h
@@ -13,6 +13,7 @@ typedef struct Timer {
     bool prev_delay;
     bool tima_has_overflowed;
     uint16_t div_value;
+    int tima_speed;
 } Timer;
 
 void gb_timer_increment(Timer* timer, MemoryBankController* mc, int cycles);
