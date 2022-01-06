bd1e534d8174efe71d4a4b306a6e0673bbdc4484
Thomas H. Evang <thomasevang@gmail.com>
2022-01-06 19:00:30 +0100

    Generic render and input init functions

--- src/Renderers/SDL2_Renderer.c
+++ src/Renderers/SDL2_Renderer.c
@@ -1,13 +1,27 @@
+#include <SDL2/SDL.h>
 #include "SDL2_Renderer.h"
 
-void sdl2_init_gui(SDL2State* gui) {
+typedef struct SDL2State {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Texture* screen;
+} SDL2State;
+
+void sdl2_render_ppu(GameBoyState* gameboy, SDL2State* gui) {
+
+    SDL_UpdateTexture(gui->screen, NULL, (void*) &(gameboy->ppu.screen.pixels[0]), 4 * 160);
+    SDL_RenderCopy(gui->renderer, gui->screen, NULL, NULL);
+}
+
+void* sdl2_init_gui() {
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("SDL ERROR: %s\n", SDL_GetError());
-        exit(1);
+        return NULL;
     }
 
-    gui->window = SDL_CreateWindow("Gameboy Emulator", 
+    SDL2State* s = malloc(sizeof(SDL2State));
+    s->window = SDL_CreateWindow("Gameboy Emulator", 
         SDL_WINDOWPOS_CENTERED, 
         SDL_WINDOWPOS_CENTERED, 
         5*160, 
@@ -15,23 +29,24 @@ void sdl2_init_gui(SDL2State* gui) {
         SDL_WINDOW_RESIZABLE);
     
 
-    if(!gui->window) {
+    if(!s->window) {
         printf("Failed to create SDL Window: %s\n", SDL_GetError());
-        exit(1);
+        return NULL;
     }
 
-    gui->renderer = SDL_CreateRenderer(
-        gui->window,
+    s->renderer = SDL_CreateRenderer(
+        s->window,
         -1,
         SDL_RENDERER_ACCELERATED
     );
 
-    if(!gui->renderer) {
+    if(!s->renderer) {
         printf("SDL ERROR: Unable to create renderer: %s\n", SDL_GetError());
-        exit(1);
+        return NULL;
     }
 
-    gui->screen = SDL_CreateTexture(gui->renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 160, 144);
+    s->screen = SDL_CreateTexture(s->renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 160, 144);
+    return s;
 }
 
 void sdl2_gb_render(GameBoyState* gameboy, void* user_data) {
@@ -41,10 +56,4 @@ void sdl2_gb_render(GameBoyState* gameboy, void* user_data) {
     SDL_RenderClear(gui->renderer);
     sdl2_render_ppu(gameboy, gui);
     SDL_RenderPresent(gui->renderer);
-}
-
-void sdl2_render_ppu(GameBoyState* gameboy, SDL2State* gui) {
-
-    SDL_UpdateTexture(gui->screen, NULL, (void*) &(gameboy->ppu.screen.pixels[0]), 4 * 160);
-    SDL_RenderCopy(gui->renderer, gui->screen, NULL, NULL);
 }
